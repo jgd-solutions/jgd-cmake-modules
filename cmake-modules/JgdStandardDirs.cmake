0bb42caca724ad9211685c7ad5248b660366c3b7
Justen Di Ruscio <justend29@gmail.com>
2021-12-31 17:11:58 -0400

    add module with standard project and install dirs

--- cmake-modules/JgdStandardDirs.cmake
+++ cmake-modules/JgdStandardDirs.cmake
@@ -1,4 +1,51 @@
+# Project Directories
 set(JGD_PROJECT_CMAKE_DIR "${PROJECT_SOURCE_DIR}/cmake")
 set(JGD_PROJECT_DATA_DIR "${PROJECT_SOURCE_DIR}/data")
 set(JGD_PROJECT_TESTS_DIR "${PROJECT_SOURCE_DIR}/tests")
 set(JGD_PROJECT_DOCS_DIR "${PROJECT_SOURCE_DIR}/docs")
+
+# Install Directories
+include(GNUInstallDirs)
+if(${PROJECT_NAME}_VERSION)
+  set(_name_version "${PROJECT_NAME}-${${PROJECT_NAME}_VERSION}")
+else()
+  set(_name_version "${PROJECT_NAME}")
+endif()
+
+set(JGD_INSTALL_CMAKE_DESTINATION
+    "${CMAKE_INSTALL_DATAROOTDIR}/cmake/${_name_version}")
+set(JGD_INSTALL_INTERFACE_INCLUDE_DIR
+    "${CMAKE_INSTALL_INCLUDEDIR}/${_name_version}")
+
+#
+# Sets the variable specified by OUT_VAR to the installation directory
+# destination for public header files. Defined as a function because the path
+# depends on the component, if any, and the presence of version.
+#
+# Arguments:
+#
+# COMPONENT: one-value arg; the component to which the headers that will be
+# installed in this location belong. Optional
+#
+# OUT_VAR: one-value arg; the name of the output variable which will store the
+# header installation directory.
+#
+function(jgd_install_include_destination)
+  jgd_parse_arguments(ONE_VALUE_KEYWORDS "OUT_VAR;COMPONENT" ARGUMENTS
+                      "${ARGN}")
+  jgd_validate_arguments(KEYWORDS "OUT_VAR")
+
+  set(include_dir "${JGD_INSTALL_INTERFACE_INCLUDE_DIR}")
+
+  if(NOT "${_name_version}" STREQUAL "${PROJECT_NAME}")
+    string(APPEND include_dir "/${PROJECT_NAME}")
+  endif()
+
+  if(ARGS_COMPONENT AND (NOT "${ARGS_COMPONENT}" STREQUAL "${PROJECT_NAME}"))
+    string(APPEND include_dir "/${ARGS_COMPONENT}")
+  endif()
+
+  set(${ARGS_OUT_VAR}
+      "${include_dir}"
+      PARENT_SCOPE)
+endfunction()
