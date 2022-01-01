617e09182e68b0ab93cc7341afb703b7f5f69495
Justen Di Ruscio <justend29@gmail.com>
2022-01-01 12:12:03 -0400

    make project arg optional in add default source subdirs

--- cmake-modules/JgdAddDefaultSourceSubdirectories.cmake
+++ cmake-modules/JgdAddDefaultSourceSubdirectories.cmake
@@ -14,7 +14,8 @@ include(JgdValidateArguments)
 #
 # Arguments:
 #
-# PROJECT: one value arg; the name of the project.
+# PROJECT: one value arg; the name of the project. Optional - if not provided
+# PROJECT_NAME will be used.
 #
 # COMPONENTS: multi value arg; list of components that the PROJECT encapsulates.
 # Optional and shouldn't be used if the project doesn't contain any components.
@@ -22,58 +23,53 @@ include(JgdValidateArguments)
 function(jgd_add_default_source_subdirectories)
   jgd_parse_arguments(ONE_VALUE_KEYWORDS "PROJECT" MULTI_VALUE_KEYWORDS
                       "COMPONENTS" ARGUMENTS "${ARGN}")
+  jgd_validate_arguments()
 
-  jgd_validate_arguments(KEYWORDS "PROJECT")
-
-  # Validate environment
-  if(NOT ${PROJECT_NAME} STREQUAL ${ARGS_PROJECT})
-    message(
-      FATAL_ERROR
-        "Project provided to ${CMAKE_CURRENT_FUNCTION}, ${ARGS_PROJECT} "
-        "doesn't match current CMake project.")
+  set(project "${PROJECT_NAME}")
+  if(ARGS_PROJECT)
+    set(project "${ARGS_PROJECT}")
   endif()
 
   # Add Source File Subdirectories
   if(DEFINED ARGS_COMPONENTS)
     # add all components' subdirectories
     foreach(component ${ARGS_COMPONENTS})
-      string(JOIN "-" comp_dir ${ARGS_PROJECT} ${component})
+      string(JOIN "-" comp_dir ${project} ${component})
       set(subdir_path
-          "${PROJECT_SOURCE_DIR}/${comp_dir}/${ARGS_PROJECT}/${component}")
+          "${${project}_SOURCE_DIR}/${comp_dir}/${project}/${component}")
       if(IS_DIRECTORY "${subdir_path}")
         message(DEBUG "${CMAKE_CURRENT_FUNCTION}: Adding directory "
-                "${subdir_path} to project ${PROJECT_NAME}")
+                "${subdir_path} to project ${project}")
         add_subdirectory(${subdir_path})
       else()
         message(
           FATAL_ERROR
             "In ${CMAKE_CURRENT_FUNCTION}. Cannot add subdirectory "
             "${subdir_path} for component ${component} of project "
-            "${ARGS_PROJECT}. Directory does not exist.")
+            "${project}. Directory does not exist.")
       endif()
     endforeach()
   else()
     # add single project source subdirectory
-    set(subdir_path "${PROJECT_SOURCE_DIR}/${ARGS_PROJECT}")
+    set(subdir_path "${${project}_SOURCE_DIR}/${project}")
     if(IS_DIRECTORY "${subdir_path}")
       message(DEBUG "${CMAKE_CURRENT_FUNCTION}: Adding directory "
-              "${subdir_path} to project ${PROJECT_NAME}")
+              "${subdir_path} to project ${project}")
       add_subdirectory(${subdir_path})
     else()
       message(
         FATAL_ERROR
           "In ${CMAKE_CURRENT_FUNCTION}. Cannot add subdirectory "
-          "${subdir_path} for project ${ARGS_PROJECT}. Directory does "
-          "not exist.")
+          "${subdir_path} for project ${project}. Directory does " "not exist.")
     endif()
 
     # add additional library source subdirectory, if project is executable
-    string(REGEX REPLACE "^lib" "" no_lib "${ARGS_PROJECT}")
-    if("${no_lib}" STREQUAL "${ARGS_PROJECT}")
-      set(subdir_path "${PROJECT_SOURCE_DIR}/lib${ARGS_PROJECT}")
+    string(REGEX REPLACE "^lib" "" no_lib "${project}")
+    if("${no_lib}" STREQUAL "${project}")
+      set(subdir_path "${${project}_SOURCE_DIR}/lib${project}")
       if(IS_DIRECTORY "${subdir_path}")
         message(DEBUG "${CMAKE_CURRENT_FUNCTION}: Adding directory "
-                "${subdir_path} to project ${PROJECT_NAME}")
+                "${subdir_path} to project ${project}")
         add_subdirectory(${subdir_path})
       endif()
     endif()
