df6a08b65d2c1885991f1d796dfb6b7b9c1f3f3e
Justen Di Ruscio <justend29@gmail.com>
2022-01-01 12:13:54 -0400

    remove literal file name in all modules

--- cmake-modules/JgdAllModules.cmake
+++ cmake-modules/JgdAllModules.cmake
@@ -1,5 +1,5 @@
 file(GLOB cmake_modules "*.cmake")
-list(REMOVE_ITEM cmake_modules JgdAllModules.cmake)
+list(REMOVE_ITEM cmake_modules ${CMAKE_CURRENT_LIST_FILE})
 
 foreach(module ${cmake_modules})
   include(${module})
