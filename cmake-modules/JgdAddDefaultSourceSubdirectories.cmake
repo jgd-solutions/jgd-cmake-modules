573ac2abdf457d2e7524bde37e85f4f2f974aa28
Justen Di Ruscio <justend29@gmail.com>
2022-01-01 21:15:39 -0400

    remove project arg - use PROJECT_NAME

--- cmake-modules/JgdAddDefaultSourceSubdirectories.cmake
+++ cmake-modules/JgdAddDefaultSourceSubdirectories.cmake
@@ -2,7 +2,7 @@ include(JgdParseArguments)
 include(JgdValidateArguments)
 
 # Adds subdirectories following JGD's C++ project layout conventions, based on
-# the PROJECT and COMPONENTS arguments. That is, the relative directories
+# the PROJECT_NAME and COMPONENTS argument. That is, the relative directories
 # ./<project>-<component>/<project>/<component> or just ./<project> and possibly
 # ./lib<project> are added as a subdirectories, depending upon if the project
 # has components or not. These paths are relative to the location of the calling
@@ -14,62 +14,55 @@ include(JgdValidateArguments)
 #
 # Arguments:
 #
-# PROJECT: one value arg; the name of the project. Optional - if not provided
-# PROJECT_NAME will be used.
-#
 # COMPONENTS: multi value arg; list of components that the PROJECT encapsulates.
 # Optional and shouldn't be used if the project doesn't contain any components.
 #
 function(jgd_add_default_source_subdirectories)
-  jgd_parse_arguments(ONE_VALUE_KEYWORDS "PROJECT" MULTI_VALUE_KEYWORDS
-                      "COMPONENTS" ARGUMENTS "${ARGN}")
+  jgd_parse_arguments(MULTI_VALUE_KEYWORDS "COMPONENTS" ARGUMENTS "${ARGN}")
   jgd_validate_arguments()
-
-  set(project "${PROJECT_NAME}")
-  if(ARGS_PROJECT)
-    set(project "${ARGS_PROJECT}")
-  endif()
 
   # Add Source File Subdirectories
   if(DEFINED ARGS_COMPONENTS)
     # add all components' subdirectories
     foreach(component ${ARGS_COMPONENTS})
-      string(JOIN "-" comp_dir ${project} ${component})
+      string(JOIN "-" comp_dir ${PROJECT_NAME} ${component})
       set(subdir_path
-          "${${project}_SOURCE_DIR}/${comp_dir}/${project}/${component}")
+          "${${PROJECT_NAME}_SOURCE_DIR}/${comp_dir}/${PROJEC_NAME}/${component}"
+      )
       if(IS_DIRECTORY "${subdir_path}")
         message(DEBUG "${CMAKE_CURRENT_FUNCTION}: Adding directory "
-                "${subdir_path} to project ${project}")
+                "${subdir_path} to project ${PROJECT_NAME}")
         add_subdirectory(${subdir_path})
       else()
         message(
           FATAL_ERROR
             "In ${CMAKE_CURRENT_FUNCTION}. Cannot add subdirectory "
             "${subdir_path} for component ${component} of project "
-            "${project}. Directory does not exist.")
+            "${PROJECT_NAME}. Directory does not exist.")
       endif()
     endforeach()
   else()
     # add single project source subdirectory
-    set(subdir_path "${${project}_SOURCE_DIR}/${project}")
+    set(subdir_path "${${PROJECT_NAME}_SOURCE_DIR}/${PROJECT_NAME}")
     if(IS_DIRECTORY "${subdir_path}")
       message(DEBUG "${CMAKE_CURRENT_FUNCTION}: Adding directory "
-              "${subdir_path} to project ${project}")
+              "${subdir_path} to project ${PROJECT_NAME}")
       add_subdirectory(${subdir_path})
     else()
       message(
         FATAL_ERROR
           "In ${CMAKE_CURRENT_FUNCTION}. Cannot add subdirectory "
-          "${subdir_path} for project ${project}. Directory does " "not exist.")
+          "${subdir_path} for project ${PROJECT_NAME}. Directory does not "
+          "exist.")
     endif()
 
     # add additional library source subdirectory, if project is executable
-    string(REGEX REPLACE "^lib" "" no_lib "${project}")
-    if("${no_lib}" STREQUAL "${project}")
-      set(subdir_path "${${project}_SOURCE_DIR}/lib${project}")
+    string(REGEX REPLACE "^lib" "" no_lib "${PROJECT_NAME}")
+    if("${no_lib}" STREQUAL "${PROJECT_NAME}")
+      set(subdir_path "${${PROJECT_NAME}_SOURCE_DIR}/lib${PROJECT_NAME}")
       if(IS_DIRECTORY "${subdir_path}")
         message(DEBUG "${CMAKE_CURRENT_FUNCTION}: Adding directory "
-                "${subdir_path} to project ${project}")
+                "${subdir_path} to project ${PROJECT_NAME}")
         add_subdirectory(${subdir_path})
       endif()
     endif()
