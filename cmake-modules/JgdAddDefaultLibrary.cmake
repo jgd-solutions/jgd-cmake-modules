cc210d90b76ea513c91209b1646fbb40ae4c2301
Justen Di Ruscio <justend29@gmail.com>
2022-01-01 23:01:02 -0400

    opt lib type & check prefix b4 setting lib out name

--- cmake-modules/JgdAddDefaultLibrary.cmake
+++ cmake-modules/JgdAddDefaultLibrary.cmake
@@ -18,26 +18,30 @@ include(JgdDefaultTargetProps)
 # LIBRARY: one value arg; the name of the library to generate.
 #
 # TYPE: one value arg; the type of the library to generate. One of STATIC,
-# SHARED, OBJECT, INTERFACE.
+# SHARED, OBJECT, INTERFACE. Optional - if omitted, the value of
+# BUILD_SHARED_LIBS will be used (one of STATIC or SHARED).
 #
 # SOURCES: multi value arg; the sources to create LIBRARY from.
 #
 function(jgd_add_default_library)
   jgd_parse_arguments(ONE_VALUE_KEYWORDS "LIBRARY;TYPE" MULTI_VALUE_KEYWORDS
                       "SOURCES" ARGUMENTS "${ARGN}")
-  jgd_validate_arguments(KEYWORDS "LIBRARY;TYPE;SOURCES")
+  jgd_validate_arguments(KEYWORDS "LIBRARY;SOURCES")
 
-  set(supported_types STATIC SHARED OBJECT INTERFACE)
-  list(FIND supported_types "${ARGS_TYPE}" supported)
-  if(supported STREQUAL -1)
-    message(
-      FATAL_ERROR
-        "Unsupported type ${ARGS_TYPE}. ${CMAKE_CURRENT_FUNCTION} must be "
-        "called with one of: ${supported_types}")
+  if(ARGS_TYPE)
+    set(supported_types STATIC SHARED OBJECT INTERFACE)
+    list(FIND supported_types "${ARGS_TYPE}" supported)
+    if(supported EQUAL -1)
+      message(
+        FATAL_ERROR
+          "Unsupported type ${ARGS_TYPE}. ${CMAKE_CURRENT_FUNCTION} must be "
+          "called with one of: ${supported_types}")
+    endif()
+  else()
+
   endif()
 
   if("${ARGS_TYPE}" STREQUAL "INTERFACE")
-    message(STATUS)
     add_library("${ARGS_LIBRARY}" ${ARGS_TYPE})
   else()
     add_library("${ARGS_LIBRARY}" ${ARGS_TYPE} ${sources_keyword}
@@ -47,9 +51,12 @@ function(jgd_add_default_library)
   # alias so library can be used as component inside build tree (tests)
   add_library(${PROJECT_NAME}::${ARGS_LIBRARY} ALIAS ${ARGS_LIBRARY})
 
-  if("${ARGS_TYPE}" STREQUAL "STATIC" OR "${ARGS_TYPE}" STREQUAL "SHARED")
-    string(REGEX REPLACE "^lib" "" outname "${ARGS_LIBRARY}")
-    set_target_properties("${ARGS_LIBRARY}" PROPERTIES OUTPUT_NAME ${outname})
+  if("${ARGS_TYPE}" MATCHES "STATIC|SHARED")
+    get_target_property(prefix "${ARGS_LIBRARY}" PREFIX)
+    if("${prefix}" STREQUAL "lib")
+      string(REGEX REPLACE "^lib" "" outname "${ARGS_LIBRARY}")
+      set_target_properties("${ARGS_LIBRARY}" PROPERTIES OUTPUT_NAME ${outname})
+    endif()
   endif()
 
   set(include_access "PUBLIC")
