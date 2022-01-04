4f58231df035670270bb12fdea7778c469e2a1e0
Justen Di Ruscio <justend29@gmail.com>
2022-01-04 15:22:28 -0400

    add basic test project

---
+++ tests/single-exec-project/single-exec-project/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <single-exec-project/single-exec-project_config.hpp>
+#include <single-exec-project/test.hpp>
+
+int main() {
+  std::cout << data_dir << std::endl;
+  return test();
+}
