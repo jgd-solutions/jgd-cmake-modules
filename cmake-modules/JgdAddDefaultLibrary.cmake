d261b5889dfe171a3c08c7d1f394a548befd33bd
Justen Di Ruscio <justend29@gmail.com>
2022-01-04 17:47:53 -0400

    refactor add lib to generate lib name

--- cmake-modules/JgdAddDefaultLibrary.cmake
+++ cmake-modules/JgdAddDefaultLibrary.cmake
@@ -3,34 +3,53 @@ include_guard()
 include(JgdParseArguments)
 include(JgdValidateArguments)
 include(JgdDefaultTargetProps)
+include(JgdCanonicalStructure)
 include(JgdFileNaming)
 
 #
-# Creates a library called LIBRARY, of the specified TYPE, with the default
-# target properties, specified in JgdDefaultTargetProps. Additionally, an ALIAS
-# library for use within the build tree. Lastly, if the library is prefixed with
-# "lib", is STATIC or SHARED, and "lib" is the library's PREFIX property, "lib"
-# is stripped from the library's output name.
+# Creates a library with a generated name and the default target properties
+# defined in JgdDefaultTargetProps. The generated name will be
+# <JGD_LIB_PREFIX><NAME>[-COMPONENT], where NAME is PROJECT_NAME with any
+# JGD_LIB_PREFIX stripped, so all libraries are prefixed by a single
+# JGD_LIB_PREFIX. Ex. 1: libproj Ex. 2: libproj-core.
+#
+# Additionally, an ALIAS library is created for use within the build tree
+# (testing), regardless of if COMPONENT is provided. The alias will be
+# <PROJECT_NAME>::<LIB>, where LIB is either the generated library name or
+# LIBRARY, if overridden. That is, unless PROJECT_NAME starts with
+# JGD_LIB_PREFIX and COMPONENT is provied, where the alias will be
+# <PROJECT_NAME>::<COMPONENT>. Ex 1. libproj::libproj Ex 2. libproj::core Ex 3.
+# proj::libproj Ex 4. proj::libproj-core
+#
+# Lastly, if the library is prefixed with JGD_LIB_PREFIX, is STATIC or SHARED,
+# and JGD_LIB_PREFIX is the same as library's PREFIX property, JGD_LIB_PREFIX is
+# stripped from the library's output name, since the PREFIX property will be
+# prepended to the output name, automatically.
 #
 # Arguments:
 #
-# LIBRARY: one-value arg; the name of the library to generate.
+# COMPONENT: one-value arg; the component of the project that the library
+# constitutes, if the project is a multi-component project. A COMPONENT that
+# matches the PROJECT_NAME will be ignored. Optional.
+#
+# LIBRARY: one-value arg; the override library name to create. Optional - if
+# omitted, a library name will be generated.
 #
 # TYPE: one-value arg; the type of the library to generate. One of STATIC,
 # SHARED, OBJECT, INTERFACE. Optional - if omitted, the generated library will
 # be one of STATIC or SHARED, determined by CMake.
 #
-# COMPONENT: one-value arg; the component in which the added library belongs.
-# Used to resolve appropriate include directories. Optional, if not part of a
-# component.
-#
 # SOURCES: multi value arg; the sources to create LIBRARY from.
 #
 function(jgd_add_default_library)
-  jgd_parse_arguments(ONE_VALUE_KEYWORDS "LIBRARY;TYPE;COMPONENT"
+  jgd_parse_arguments(ONE_VALUE_KEYWORDS "COMPONENT;LIBRARY;TYPE"
                       MULTI_VALUE_KEYWORDS "SOURCES" ARGUMENTS "${ARGN}")
-  jgd_validate_arguments(KEYWORDS "LIBRARY;SOURCES")
+  jgd_validate_arguments(KEYWORDS "SOURCES")
+  if(NOT "${ARGS_COMPONENT}" STREQUAL "${PROJECT_NAME}")
+    set(component "${ARGS_COMPONENT}")
+  endif()
 
+  # Verify source naming
   foreach(source ${ARGS_SOURCES})
     set(regex "${JGD_HEADER_REGEX}|${JGD_SOURCE_REGEX}")
     string(REGEX MATCH "${regex}" matched "${source}")
@@ -40,6 +59,16 @@ function(jgd_add_default_library)
     endif()
   endforeach()
 
+  # Generate library name
+  string(REGEX REPLACE "^${JGD_LIB_PREFIX}" "" no_prefix ${PROJECT_NAME})
+  set(library "${JGD_LIB_PREFIX}${no_prefix}")
+  if(ARGS_LIBRARY)
+    set(library "${ARGS_LIBRARY}")
+  elseif(component)
+    string(APPEND library "-${component}")
+  endif()
+
+  # Override library type with TYPE, if provided and supported
   set(type)
   if(ARGS_TYPE)
     set(supported_types STATIC SHARED OBJECT INTERFACE)
@@ -53,23 +82,29 @@ function(jgd_add_default_library)
     set(type "${ARGS_TYPE}")
   endif()
 
+  # Add the library with alias
   if("${type}" STREQUAL "INTERFACE")
-    add_library("${ARGS_LIBRARY}" INTERFACE)
+    add_library("${library}" INTERFACE)
   else()
-    add_library("${ARGS_LIBRARY}" ${type} SOURCES "${ARGS_SOURCES}")
+    add_library("${library}" ${type} SOURCES "${ARGS_SOURCES}")
   endif()
 
-  # alias so library can be used as component inside build tree (tests)
-  add_library(${PROJECT_NAME}::${ARGS_LIBRARY} ALIAS ${ARGS_LIBRARY})
+  set(alias "${PROJECT_NAME}::${library}")
+  if((NOT ${no_prefix} STREQUAL ${PROJECT_NAME}) AND component)
+    set(alias "${PROJECT_NAME}::${component}")
+  endif()
+  add_library(${alias} ALIAS ${ARGS_LIBRARY})
 
+  # Change output name
   if("${type}" MATCHES "STATIC|SHARED")
     get_target_property(prefix "${ARGS_LIBRARY}" PREFIX)
     if("${prefix}" STREQUAL "${JGD_LIB_PREFIX}")
-      string(REGEX REPLACE "^${JGD_LIB_PREFIX}" "" outname "${ARGS_LIBRARY}")
-      set_target_properties("${ARGS_LIBRARY}" PROPERTIES OUTPUT_NAME ${outname})
+      set_target_properties("${ARGS_LIBRARY}" PROPERTIES OUTPUT_NAME
+                                                         ${no_prefix})
     endif()
   endif()
 
+  # Set default target properties
   set(include_access "PUBLIC")
   if("${type}" STREQUAL "INTERFACE")
     set(include_access "INTERFACE")
@@ -79,8 +114,8 @@ function(jgd_add_default_library)
   endif()
 
   set(comp_arg)
-  if(ARGS_COMPONENT)
-    set(comp_arg "COMPONENT ${ARGS_COMPONENT}")
+  if(component)
+    set(comp_arg "COMPONENT ${component}")
   endif()
   jgd_default_include_dirs(BUILD_INTERFACE ${comp_arg} OUT_VAR include_dirs)
   target_include_directories("${ARGS_LIBRARY}" ${include_access}
