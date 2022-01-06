b73a005c3ec0207c02f37d89a0b86145717d2237
Justen Di Ruscio <justend29@gmail.com>
2022-01-06 13:05:36 -0400

    add module to resolve library components

---
+++ cmake-modules/JgdGetLibraryComponent.cmake
@@ -0,0 +1,43 @@
+include_guard()
+
+include(JgdParseArguments)
+include(JgdValidateArguments)
+include(JgdCanonicalStructure)
+
+#
+# Resolves the component of the given library name specified by the LIBRARY
+# argument. The provided LIBRARY must have been created by JgdGetDefaultLibrary
+# name, or at least follow its semantics, for the component to be correctly
+# resolved. If the given library is not a component, the result will be an empty
+# string.
+#
+# Arguments:
+#
+# LIBRARY: one-value arg; the library name for which the component will be
+# resolved.
+#
+# OUT_VAR: one-value arg; the name of the variable that will store the resulting
+# component.
+#
+function(jgd_get_library_component)
+  jgd_parse_arguments(ONE_VALUE_KEYWORDS "LIBRARY;OUT_VAR" ARGUMENTS
+                      "${ARGN}")
+  jgd_validate_arguments(KEYWORDS "LIBRARY;OUT_VAR")
+
+  set(component)
+
+  # Resolve component from library name
+  set(exe_comp_lib_prefix "^${JGD_LIB_PREFIX}${PROJECT_NAME}-")
+  if("${ARGS_LIBRARY}" MATCHES "^${exe_comp_lib_prefix}")
+    # libraries that start with the executable component library prefix are
+    # followed by their component
+    string(REPLACE "${exe_comp_lib_prefix}" "" component "${ARGS_LIBRARY}")
+
+  elseif(NOT "${ARGS_LIBRARY}" MATCHES "^${JGD_LIB_PREFIX}")
+    # libraries that don't start with JGD_LIB_PREFIX are component libraries of the same name
+    set(component "${ARGS_LIBRAY}")
+  endif()
+
+  # Set result
+  set(${ARGS_OUT_VAR} "${component}" PARENT_SCOPE)
+endif()
