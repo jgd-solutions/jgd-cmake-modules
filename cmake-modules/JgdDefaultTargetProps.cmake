2f5f590da82ef808bce1b9396cbd94979ef7a682
Justen Di Ruscio <justend29@gmail.com>
2022-01-06 14:46:10 -0400

    refactor default include dirs to use target

--- cmake-modules/JgdDefaultTargetProps.cmake
+++ cmake-modules/JgdDefaultTargetProps.cmake
@@ -3,6 +3,7 @@ include_guard()
 include(JgdParseArguments)
 include(JgdValidateArguments)
 include(JgdCanonicalStructure)
+include(JgdGetLibraryComponent)
 
 # cmake-lint: disable=C0301
 set(JGD_DEFAULT_COMPILE_OPTIONS
@@ -22,63 +23,61 @@ set(JGD_LIB_POSITION_INDEPENDENT_CODE "${BUILD_SHARED_LIBS}")
 set(JGD_LIB_PREFIX "${JGD_LIB_PREFIX}")
 
 #
-# Sets the variable specified by OUT_VAR to the default library output name.
-# This name will be <name>[-COMPONENT], where 'name' is PROJECT_NAME with any
-# leading JGD_LIB_PREFIX removed. When combined with the target's PREFIX
-# property, which should be set to JGD_LIB_PREFIX, the libraries file name on
-# disk will be <PREFIX><name>[-COMPONENT].
+# Sets the variable specified by OUT_VAR to the default library output name for
+# the provided LIBRARY.  This name will be <name>[-component], where 'name' is
+# PROJECT_NAME with any leading JGD_LIB_PREFIX removed, and 'component' is the
+# library component resolved from LIBRARY. When combined with the target's
+# PREFIX property, which should be set to JGD_LIB_PREFIX, the libraries file
+# name on disk will be <PREFIX><name>[-component].
 #
 # Arguments:
 #
-# COMPONENT: one-value arg; the library component that the library output name
-# is naming.
+# LIBRARY: one-value arg; the library target name that produces the on-disk
+# library of the resulting output name. LIBRARY must have been created by
+# JgdGetDefaultLibraryTargetName, or at least follow its semantics.
 #
 # OUT_VAR: one-value arg; the name of the variable that will store the resulting
 # output name.
 #
-function(jgd_default_lib_output_name)
-  jgd_parse_arguments(ONE_VALUE_KEYWORDS "COMPONENT;OUT_VAR" ARGUMENTS
-                      "${ARGN}")
-  jgd_validate_arguments(KEYWORDS "OUT_VAR")
+function(jgd_default_library_output_name)
+  jgd_parse_arguments(ONE_VALUE_KEYWORDS "LIBRARY;OUT_VAR" ARGUMENTS "${ARGN}")
+  jgd_validate_arguments(KEYWORDS "LIBRARY;OUT_VAR")
 
+  # Compute library component
+  jgd_get_library_component(LIBRARY "${ARGS_LIBRARY}" OUT_VAR component)
+
+  # Construct output name
   string(REGEX REPLACE "^${JGD_LIB_PREFIX}" "" no_prefix ${PROJECT_NAME})
   set(out_name "${no_prefix}")
-
-  if(ARGS_COMPONENT AND (NOT "${ARGS_COMPONENT}" STREQUAL "${PROJECT_NAME}"))
-    string(APPEND out_name "-${ARGS_COMPONENT}")
+  if(component)
+    string(APPEND out_name "-${component}")
   endif()
+
+  # Set result
   set(${ARGS_OUT_VAR}
       "${out_name}"
       PARENT_SCOPE)
 endfunction()
 
 #
-# Sets the variable specified by OUT_VAR to the default include paths within the
-# CMAKE_CURRENT_SOURCE_DIR, or SOURCE_DIR, following the canonical project
-# structure.  First, the canonical subdirectories given by JgdCanonicalStructure
-# are compared as prefixes against CMAKE_CURRENT_SOURCE_DIR/SOURCE_DIR to find
-# which canonical subdirectory the source directory is/is within. If matched, a
-# resulting path will be set such that the include prefix is always
-# <PROJECT_NAME>[/COMPONENT]/, plus the nested directories within the matched
-# canonical directory that the current directory exists within. Second, the
-# PROJECT_BINARY_DIR is added for any generated headers, which should be
-# generated in PROJECT_BINARY_DIR/PROJECT_NAME/... . The result can be
-# optionally wrapped in a BUILD_INTERFACE generator expression.
+# Sets the variable specified by OUT_VAR to the default include paths for the
+# provided TARGET. If TARGET was properly created in or within one of the
+# canonical subdirectories, provided by JgdCanonicalStructure, the resulting
+# path will be set such that the include prefix is always
+# <PROJECT_NAME>[/COMPONENT]/, plus the nested directories within the canonical
+# directory that the SOURCE_DIR exists within. PROJECT_BINARY_DIR is also added
+# for any generated headers, which should be generated in
+# PROJECT_BINARY_DIR/PROJECT_NAME/... .  The result can be optionally wrapped in
+# a BUILD_INTERFACE generator expression.
 #
-# Example: if CMAKE_CURRENT_SOURCE_DIR is path .../proj-comp/proj/comp/thing,
-# with respect to PROJECT_SOURCE_DIR, where PROJECT_NAME is "proj" and COMPONENT
-# is "comp", the source include directory will be proj/proj-comp. Then,
-# PROJECT_BINARY_DIR will be added.
+# Example: if TARGET was created in .../proj-comp/proj/comp/thing, with respect
+# to PROJECT_SOURCE_DIR, where PROJECT_NAME is "proj" and COMPONENT is "comp",
+# the source include directory will be proj/proj-comp. Then, PROJECT_BINARY_DIR
+# will be added.
 #
 # Arguments:
 #
-# SOURCE_DIR: one-value arg; The source directory to calculate include
-# directories for. Overrides CMAKE_CURRENT_SOURCE_DIR. Optional -
-# CMAKE_CURRENT_SOURCE_DIR will be used by default.
-#
-# COMPONENT: one-value arg; the component in which the current directory exists.
-# Used to compute the canonical component subdirectory. A COMPONENT matching
-# PROJECT_NAME will be ignored. Optional.
+# TARGET: one-value arg; the target to compute include directories for.
 #
 # OUT_VAR: one-value arg; the name of the variable that will store the resulting
 # path.
@@ -89,52 +88,55 @@ endfunction()
 #
 function(jgd_default_include_dirs)
   jgd_parse_arguments(OPTIONS "BUILD_INTERFACE" ONE_VALUE_KEYWORDS
-                      "COMPONENT;OUT_VAR;SOURCE_DIR" ARGUMENTS "${ARGN}")
+                      "TARGET;OUT_VAR" ARGUMENTS "${ARGN}")
   jgd_validate_arguments(KEYWORDS "OUT_VAR")
 
-  # Override source directory, if argument is provided
-  set(source_dir "${CMAKE_CURRENT_SOURCE_DIR}")
-  if(ARGS_SOURCE_DIR)
-    set(source_dir "${ARGS_SOURCE_DIR}")
-  endif()
+  # Get the target's properties
+  get_target_property(source_dir ${ARGS_TARGET} TYPE)
+  get_target_property(target_type ${ARGS_TARGET} TYPE)
 
-  # Calculate canonical subdirectories
-  jgd_canonical_exec_subdir(OUT_VAR exec_subdir)
-  jgd_canonical_lib_subdir(OUT_VAR lib_subdir)
-  if(ARGS_COMPONENT AND (NOT "${ARGS_COMPONENT}" STREQUAL "${PROJECT_NAME}"))
-    jgd_canonical_component_subdir(COMPONENT "${ARGS_COMPONENT}" OUT_VAR
-                                   comp_subdir)
-  endif()
+  # Helpful macro to check & emit error
+  macro(_CHECK_ERR subdir lib_type)
+    if(NOT "${source_dir}" MATCHES "^${subdir}")
+      message(
+        FATAL_ERROR
+          "Unable to resolve default include directory. The source directory, "
+          "${source_dir}, is not within the expected ${lib_type} subdirectory of "
+          "${subdir}.")
+    endif()
+  endmacro()
 
-  # Track which canonical subdir matches source location and the number of
-  # parent traversals to create correct include prefix
-  set(include_dir)
-  if(comp_subdir AND ("${source_dir}" MATCHES "^${comp_subdir}"))
-    set(num_parents 2)
-    set(include_dir "${comp_subdir}")
-  elseif("${source_dir}" MATCHES "^${lib_subdir}")
-    set(num_parents 1)
-    set(include_dir "${lib_subdir}")
-  elseif("${source_dir}" MATCHES "^${exec_subdir}")
-    set(num_parents 1)
+  # Set include directory from canonical directories for respective target type
+  if("${target_type}" STREQUAL "EXECUTABLE")
+    # executable
+    jgd_canonical_exec_subdir(OUT_VAR exec_subdir)
+    _check_err("${exec_subdir}" "executable")
+    set(prefix_parents 1)
     set(include_dir "${exec_subdir}")
   else()
-    if(comp_subdir)
-      set(comp_err_msg " OR ${comp_subdir}")
+    jgd_get_library_component(LIBRARY "${ARGS_TARGET}" OUT_VAR component)
+    if(component)
+      # library component
+      jgd_canonical_component_subdir(COMPONENT "${ARGS_COMPONENT}" OUT_VAR
+                                     comp_subdir)
+      _check_err("${comp_subdir}" "library component")
+      set(prefix_parents 2)
+      set(include_dir "${comp_subdir}")
+    else()
+      # main library
+      jgd_canonical_lib_subdir(OUT_VAR lib_subdir)
+      _check_err("${comp_subdir}" "library")
+      set(prefix_parents 1)
+      set(include_dir "${lib_subdir}")
     endif()
-    message(
-      FATAL_ERROR
-        "Unable to resolve default include directory. The source directory, "
-        "${source_dir}, is not within ${exec_subdir} OR ${lib_subdir}"
-        "${comp_err_msg}.")
   endif()
 
-  # set include dir up the canonical subdir path to create include prefix
-  foreach(i RANGE 1 ${num_parents})
+  # Set include dir up the canonical subdir path to create include prefix
+  foreach(i RANGE 1 ${prefix_parents})
     cmake_path(GET include_dir PARENT_PATH include_dir)
   endforeach()
 
-  # Set result
+  # Set result with binary directory
   set(include_dirs "${include_dir};${PROJECT_BINARY_DIR}")
   if(ARGS_BUILD_INTERFACE)
     set(${ARGS_OUT_VAR}
