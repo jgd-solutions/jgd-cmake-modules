bbef329ea206b09c8a5a1f511140d5940f0cd9c0
Justen Di Ruscio <justend29@gmail.com>
2022-01-28 22:21:00 -0400

    basic target props for add library

--- cmake-modules/JgdAddLibrary.cmake
+++ cmake-modules/JgdAddLibrary.cmake
@@ -1,26 +1,68 @@
 include_guard()
 
+include(JgdParseArguments)
+include(JgdFileNaming)
+include(JgdTargetNaming)
+include(JgdSeparateList)
 include(GenerateExportHeader)
 
-# check to make sure we're under a source directory, check all file names,
-# create build_shared option for component, if it's a component, set output
-# name, set prefix
+# create build_shared option for component, if it's a component
 
-function(jgd_add_default_library)
-  # generate_export_header(${library} PREFIX_NAME ${JGD_PROJECT_PREFIX_NAME}
-  # EXPORT_FILE_NAME .)
+function(jgd_add_library)
+  jgd_parse_arguments(
+    ONE_VALUE_KEYWORDS
+    "COMPONENT;LIBRARY;TYPE"
+    MULTI_VALUE_KEYWORDS
+    "SOURCES"
+    REQUIRES_ALL
+    "SOURCES"
+    ARGUMENTS
+    "${ARGN}")
 
-  # warn about component name
+  if(ARGS_COMPONENT AND NOT ARGS_COMPONENT STREQUAL PROJECT_NAME)
+    set(comp_arg COMPONENT ${ARGS_COMPONENT})
+  endif()
 
-  add_library(${PROJECT_NAME}_${library}) # to avoid clashes with other targets
-                                          # if added as a subdirectory
-  add_library(${PROJECT_NAME}::${library} ALIAS ${library})
-  set_target_properties(MyProj_Algo PROPERTIES EXPORT_NAME ${library})
+  # Verify source naming
+  set(regex "${JGD_HEADER_REGEX}|${JGD_SOURCE_REGEX}")
+  jgd_separate_list(IN_LIST "${ARGS_SOURCES}" TRANSFORM "FILENAME"
+                    OUT_UNMATCHED incorrectly_named)
+  if(incorrectly_named)
+    message(
+      FATAL_ERROR
+        "Provided source files do not match the regex for library sources, "
+        "${regex}: ${incorrectly_named}.")
+  endif()
+
+  # Library names
+  if(ARGS_LIBRARY)
+    set(target_name "${ARGS_LIBRARY}")
+    set(export_name ${library})
+    set(output_name ${library})
+  else()
+    jgd_library_target_name(${comp_arg} OUT_VAR target_name)
+    string(REPLACE "${PROJECT_NAME}_" "" export_name "${library}")
+    string(REGEX REPLACE "^${JGD_LIB_PREFIX}" "" output_name "${export_name}")
+  endif()
+
+  # Create Library
+  add_library(${target_name})
+  add_library(${PROJECT_NAME}::${export_name} ALIAS ${target_name})
+  set_target_properties(
+    ${target_name}
+    PROPERTIES OUTPUT_NAME ${output_name}
+               PREFIX ${JGD_LIB_PREFIX}
+               EXPORT_NAME ${export_name})
 
   if(PROJECT_VERSION)
     set_target_properties(
-      ${library} PROPERTIES VERSION ${PROJECT_VERSION} SOVERSION
-                                                       ${PROJECT_VERSION_MAJOR})
+      ${target_name} PROPERTIES VERSION ${PROJECT_VERSION}
+                                SOVERSION ${PROJECT_VERSION_MAJOR})
   endif()
+
+  # generate_export_header(${library} PREFIX_NAME ${JGD_PROJECT_PREFIX_NAME}
+  # EXPORT_FILE_NAME .)
+
+  # compile flags? include dirs?
 
 endfunction()
