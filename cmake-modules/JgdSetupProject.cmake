e57da914b40c3d8aaadb136cfc3b96f402df6eb4
Justen Di Ruscio <justend29@gmail.com>
2022-04-16 18:28:29 -0300

    only enable IPO in release

--- cmake-modules/JgdSetupProject.cmake
+++ cmake-modules/JgdSetupProject.cmake
@@ -188,30 +188,21 @@ function(jgd_setup_project)
     _jgd_warn_set(CMAKE_INSTALL_RPATH ${rpath_base} ${rpath_base}/${rel_path})
   endif()
 
-  macro(enable_ipo)
-    if(CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL
-                                              "RelWithDebInfo")
-      include(CheckIPOSupported)
-      check_ipo_supported(RESULT result OUTPUT output)
-      if(result)
-        set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
-      else()
-        message(SEND_ERROR "IPO is not supported: ${output}")
-      endif()
-    endif()
-  endmacro()
-
   # interprocedural/link-time optimization
 
-  check_ipo_supported(RESULT ipo_supported OUTPUT err_msg)
-  if(ipo_supported)
-    _jgd_warn_set(CMAKE_INTERPROCEDURAL_OPTIMIZATION
-                  $<IF:$<CONFIG:DEBUG>,OFF,ON>)
-  else()
-    _jgd_warn_set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF)
-    message(NOTICE
-            "Interprocedural linker optimization is not supported: ${err_msg}\n"
-            "Continuing without it.")
+  if((NOT languages STREQUAL "NONE") AND (CMAKE_BUILD_TYPE MATCHES
+                                          "Release|RelWithDepInfo"))
+    check_ipo_supported(RESULT ipo_supported OUTPUT err_msg)
+    if(ipo_supported)
+      _jgd_warn_set(CMAKE_INTERPROCEDURAL_OPTIMIZATION
+                    $<IF:$<CONFIG:DEBUG>,OFF,ON>)
+    else()
+      _jgd_warn_set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF)
+      message(
+        NOTICE
+        "Interprocedural linker optimization is not supported: ${err_msg}\n"
+        "Continuing without it.")
+    endif()
   endif()
 
   # == Variables Controlling CMake ==
