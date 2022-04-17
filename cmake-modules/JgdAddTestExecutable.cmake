887608b6c1c4cde52e94ea4b9d164f6a51138561
Justen Di Ruscio <justend29@gmail.com>
2022-04-17 15:45:32 -0300

    check build tests option b4 adding test exec

--- cmake-modules/JgdAddTestExecutable.cmake
+++ cmake-modules/JgdAddTestExecutable.cmake
@@ -10,7 +10,8 @@ include(JgdStandardDirs)
 # command, while also setting default target compile options from
 # JgdDefaultCompileOptions. An executable with name EXECUTABLE will be created
 # from the sources provided to SOURCES. This executable will then be registered
-# as a test with name NAME, or EXECUTABLE, if NAME is not provided.
+# as a test with name NAME, or EXECUTABLE, if NAME is not provided. This
+# function has no affect if <JGD_PROJECT_PREFIX_NAME>_BUILD_TESTS is not set.
 #
 # Arguments:
 #
@@ -35,28 +36,32 @@ function(jgd_add_test_executable)
     ARGUMENTS
     "${ARGN}")
 
+  if (NOT ${JGD_PROJECT_PREFIX_NAME}_BUILD_TESTS)
+    return()
+  endif ()
+
   # Verify source naming
 
-  if("${CMAKE_CURRENT_SOURCE_DIR}" MATCHES "^${JGD_PROJECT_TESTS_DIR}")
+  if ("${CMAKE_CURRENT_SOURCE_DIR}" MATCHES "^${JGD_PROJECT_TESTS_DIR}")
     set(test_source_regex "${JGD_SOURCE_REGEX}") # other tests & drivers, only
-  else()
+  else ()
     set(test_source_regex "${JGD_TEST_SOURCE_REGEX}") # unit test files, only
-  endif()
+  endif ()
 
   set(regex "${JGD_HEADER_REGEX}|${test_source_regex}")
-  foreach(source ${ARGS_SOURCES})
+  foreach (source ${ARGS_SOURCES})
     string(REGEX MATCH "${regex}" matched "${source}")
-    if(NOT matched)
+    if (NOT matched)
       message(FATAL_ERROR "Provided source file, ${source}, does not match the"
-                          "regex for test executable sources, ${regex}.")
-    endif()
-  endforeach()
+        "regex for test executable sources, ${regex}.")
+    endif ()
+  endforeach ()
 
   # Default test name
   set(test_name "${ARGS_EXECUTABLE}")
-  if(DEFINED ARGS_NAME)
+  if (DEFINED ARGS_NAME)
     set(test_name "${ARGS_NAME}")
-  endif()
+  endif ()
 
   # Test executable
   add_executable(${ARGS_EXECUTABLE} "${ARGS_SOURCES}")
@@ -66,6 +71,6 @@ function(jgd_add_test_executable)
   set_target_properties(
     ${ARGS_EXECUTABLE}
     PROPERTIES OUTPUT_NAME ${ARGS_EXECUTABLE}
-               COMPILE_OPTIONS ${JGD_DEFAULT_COMPILE_OPTIONS}
-               LINK_LIBRARIES "${ARGS_LIBS}")
+    COMPILE_OPTIONS ${JGD_DEFAULT_COMPILE_OPTIONS}
+    LINK_LIBRARIES "${ARGS_LIBS}")
 endfunction()
