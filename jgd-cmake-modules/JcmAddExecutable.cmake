3c325c8a1ec4b456a21a7f83879b76ed95941c47
Justen Di Ruscio <justend29@gmail.com>
2022-09-08 20:37:59 -0300

    document jcmaddexecutable

--- jgd-cmake-modules/JcmAddExecutable.cmake
+++ jgd-cmake-modules/JcmAddExecutable.cmake
@@ -1,5 +1,12 @@
 include_guard()
 
+#[=======================================================================[.rst:
+
+JcmAddExecutable
+================
+
+#]=======================================================================]
+
 include(JcmParseArguments)
 include(JcmFileNaming)
 include(JcmTargetNaming)
@@ -7,13 +14,109 @@ include(JcmListTransformations)
 include(JcmCanonicalStructure)
 include(JcmDefaultCompileOptions)
 
-# again, artifacts
+
+#[=======================================================================[.rst:
+---------------------------------------------
+
+.. cmake:command:: jcm_add_executable
+
+  .. code-block:: cmake
+
+    jcm_add_executable(
+      [WITHOUT_CANONICAL_PROJECT_CHECK]
+      [COMPONENT <component>]
+      [EXECUTABLE <executable>]
+      [OUT_TARGET_NAME <out-var>]
+      [OBJ_SOURCES <source>...]
+      SOURCES <source>...
+    )
+
+---------------------------------------------
+
+Adds an executable target to the project, similar to CMake's `add_executable`, but with enhancements
+. It allows creating both the executable and, optionally, an associated object library to
+allow better automated testing of the executable's sources. This object library will have the same
+name as the executable, but with '-objects' appended (main -> main-objects).
+
+This function will:
+
+- ensure it's called within a canonical source subdirectory and verify the naming conventions of the
+  input source files
+- automatically be transform all sources provided to SOURCES and OBJ_SOURCES to absolute paths.
+- create an executable target with :cmake:command:`add_executable`, including an associated alias
+  (<PROJECT_NAME>::<target>) - both following JCM's target naming conventions
+- set target properties
+    - OUTPUT_NAME
+    - EXPORT_NAME
+    - COMPILE_OPTIONS
+    - INCLUDE_DIRECTORIES
+    - COMPONENT (custom property to JCM)
+
+Parameters
+##########
+
+Options
+~~~~~~~~~~
+
+:cmake:variable:`WITHOUT_CANONICAL_PROJECT_CHECK`
+  When provided, will forgo the default check that the function is called within an executable
+  source subdirectory, as defined by the `Canonical Project Structure`_
+
+One Value
+~~~~~~~~~~
+
+:cmake:variable:`COMPONENT`
+  Specifies the component that this executable represents. Used to set `COMPONENT` property and when
+  naming the target
+
+:cmake:variable:`EXECUTABLE`
+  Overrides the target name, output name, and exported name from those automatically created to
+  conform to JCM's naming conventions
+
+:cmake:variable:`OUT_TARGET_NAME`
+  The variable named will be set to the created target's name
+
+Multi Value
+~~~~~~~~~~~
+
+:cmake:variable:`OBJ_SOURCES`
+  Sources used to create the executable's associated object library. When provided, an object
+  library will be created, it will be linked against the executable, and its include directories
+  will be set (PUBLIC) instead of the executable's. This object library will have the same name as the
+  executable, but with '-objects' appended (main -> main-objects).
+
+:cmake:variable:`SOURCES`
+  Sources used to create the executable
+
+Examples
+########
+
+.. code-block:: cmake
+
+  jcm_add_executable(SOURCES main.cpp)
+  target_link_libraries(main::main PRIVATE libthird::party)
+
+.. code-block:: cmake
+
+  jcm_add_executable(
+    OUT_TARGET_NAME target
+    SOURCES main.cpp
+    OBJ_SOURCES engine.cpp
+  )
+
+  jcm_add_test_executable(
+    NAME test_engine
+    SOURCES test_engine.cpp
+    LIBS ${target}-objects Boost::ut
+  )
+
+#]=======================================================================]
 function(jcm_add_executable)
   jcm_parse_arguments(
     OPTIONS "WITHOUT_CANONICAL_PROJECT_CHECK"
     ONE_VALUE_KEYWORDS "COMPONENT;EXECUTABLE;OUT_TARGET_NAME"
-    MULTI_VALUE_KEYWORDS "SOURCES;MAIN_SOURCES"
-    REQUIRES_ALL "MAIN_SOURCES"
+    MULTI_VALUE_KEYWORDS "SOURCES;OBJ_SOURCES"
+    REQUIRES_ALL "SOURCES"
     ARGUMENTS "${ARGN}")
 
   # Set executable component
@@ -41,7 +144,7 @@ function(jcm_add_executable)
   # verify source naming
   set(regex "${JCM_HEADER_REGEX}|${JCM_SOURCE_REGEX}")
   jcm_separate_list(
-    IN_LIST "${ARGS_SOURCES};${ARGS_MAIN_SOURCES}"
+    IN_LIST "${ARGS_SOURCES};${ARGS_OBJ_SOURCES}"
     REGEX "${regex}"
     TRANSFORM "FILENAME"
     OUT_UNMATCHED incorrectly_named
@@ -73,8 +176,8 @@ function(jcm_add_executable)
   endif ()
 
   # create executable target
-  jcm_transform_list(TRANSFORM "ABSOLUTE_PATH" INPUT "${ARGS_MAIN_SOURCES}" OUT_VAR abs_main_sources)
-  add_executable(${target_name} "${abs_main_sources}")
+  jcm_transform_list(TRANSFORM "ABSOLUTE_PATH" INPUT "${ARGS_SOURCES}" OUT_VAR abs_sources)
+  add_executable(${target_name} "${abs_sources}")
   add_executable(${PROJECT_NAME}::${export_name} ALIAS ${target_name})
 
   # == Set Target Properties ==
@@ -88,7 +191,7 @@ function(jcm_add_executable)
     COMPILE_OPTIONS "${JCM_DEFAULT_COMPILE_OPTIONS}")
 
   # include directories, if no object library will be created to provide them
-  if (NOT DEFINED ARGS_SOURCES)
+  if (NOT DEFINED ARGS_OBJ_SOURCES)
     target_include_directories(${target_name} PRIVATE "$<BUILD_INTERFACE:${include_dirs}>")
   endif ()
 
@@ -100,9 +203,9 @@ function(jcm_add_executable)
   # == Object Library ==
 
   # create library of exec's objects, allowing unit testing of exec's sources
-  if (DEFINED ARGS_SOURCES)
-    jcm_transform_list(TRANSFORM "ABSOLUTE_PATH" INPUT "${ARGS_SOURCES}" OUT_VAR abs_sources)
-    add_library(${target_name}-objects OBJECT "${abs_sources}")
+  if (DEFINED ARGS_OBJ_SOURCES)
+    jcm_transform_list(TRANSFORM "ABSOLUTE_PATH" INPUT "${ARGS_OBJ_SOURCES}" OUT_VAR abs_obj_sources)
+    add_library(${target_name}-objects OBJECT "${abs_obj_sources}")
 
     # properties on executable objects
     target_compile_options(${target_name}-objects PRIVATE "${JCM_DEFAULT_COMPILE_OPTIONS}")
