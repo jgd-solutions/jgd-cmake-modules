231951b6b232d530f5f9ca8de55831711b1f86a3
Justen Di Ruscio <justend29@gmail.com>
2022-10-30 16:48:07 -0300

    setup test dependencies and resource locks for parallel exec.

--- tests/testingFunctions.cmake
+++ tests/testingFunctions.cmake
@@ -6,9 +6,8 @@ set(jgd-cmake-modules_ROOT "${CMAKE_CURRENT_BINARY_DIR}/install")
 function(_build_and_ctest project_name)
   jcm_parse_arguments(
     ONE_VALUE_KEYWORDS "NAME_SUFFIX" "BUILD_TARGET"
-    MULTI_VALUE_KEYWORDS "BUILD_OPTIONS"
-    ARGUMENTS "${ARGN}"
-  )
+    MULTI_VALUE_KEYWORDS "BUILD_OPTIONS" "DEPENDS"
+    ARGUMENTS "${ARGN}")
 
   if (NOT ARGS_BUILD_TARGET)
     set(ARGS_BUILD_TARGET all)
@@ -17,8 +16,10 @@ function(_build_and_ctest project_name)
     unset(test_arg)
   endif ()
 
+  set(test_name "${project_name}${ARGS_NAME_SUFFIX}")
+
   add_test(
-    NAME "${project_name}${ARGS_NAME_SUFFIX}"
+    NAME ${test_name}
     COMMAND
     "${CMAKE_CTEST_COMMAND}"
     --verbose
@@ -29,28 +30,41 @@ function(_build_and_ctest project_name)
     --build-and-test "${CMAKE_CURRENT_SOURCE_DIR}/${project_name}" "${CMAKE_CURRENT_BINARY_DIR}/${project_name}"
     --build-options "-Djgd-cmake-modules_ROOT:PATH=${jgd-cmake-modules_ROOT}" "-DCMAKE_VERBOSE_MAKEFILE=ON" ${ARGS_BUILD_OPTIONS}
     ${test_arg})
+
+  set_tests_properties(${test_name} PROPERTIES RESOURCE_LOCK "build-${project_name}")
+
+  if (DEFINED ARGS_DEPENDS)
+    set_tests_properties(${test_name} PROPERTIES DEPENDS "${ARGS_DEPENDS}")
+  endif ()
 endfunction()
 
 
 function(_install_project project_name)
+  jcm_parse_arguments(MULTI_VALUE_KEYWORDS "DEPENDS" ARGUMENTS "${ARGN}")
+
   set(${project_name}_ROOT "${CMAKE_CURRENT_BINARY_DIR}/${project_name}/install")
   set(${project_name}_ROOT "${${project_name}_ROOT}" PARENT_SCOPE)
+  set(test_name ${project_name}-install)
 
   add_test(
-    NAME ${project_name}-install
+    NAME ${test_name}
     COMMAND
     "${CMAKE_COMMAND}"
     --install "${CMAKE_CURRENT_BINARY_DIR}/${project_name}"
     --verbose
     --prefix "${${project_name}_ROOT}")
+
+  if (DEFINED ARGS_DEPENDS)
+    set_tests_properties(${test_name} PROPERTIES DEPENDS "${ARGS_DEPENDS}")
+  endif ()
 endfunction()
 
 
 function(_find_use_project project_name)
   jcm_parse_arguments(
-    MULTI_VALUE_KEYWORDS "COMPONENTS"
-    ARGUMENTS "${ARGN}"
-  )
+    ONE_VALUE_KEYWORDS "OUT_TEST_NAME"
+    MULTI_VALUE_KEYWORDS "COMPONENTS" "DEPENDS"
+    ARGUMENTS "${ARGN}")
 
   if (ARGS_COMPONENTS)
     set(specified_components "${ARGS_COMPONENTS}")
@@ -62,9 +76,10 @@ function(_find_use_project project_name)
   endif ()
 
   set(${project_name}_ROOT "${CMAKE_CURRENT_BINARY_DIR}/${project_name}/install")
+  set(test_name "${project_name}-find-use${test_name_suffix}")
 
   add_test(
-    NAME ${project_name}-find-use${test_name_suffix}
+    NAME ${test_name}
     COMMAND
     "${CMAKE_CTEST_COMMAND}"
     --verbose
@@ -80,14 +95,26 @@ function(_find_use_project project_name)
     "-D ${project_name}_ROOT:PATH=${${project_name}_ROOT}"
     "-D test_name=find-use-${project_name}"
     "-D components=${specified_components}"
-    --test-command "${CMAKE_CTEST_COMMAND}"
-  )
+    --test-command "${CMAKE_CTEST_COMMAND}")
+
+  set_tests_properties(${test_name} PROPERTIES RESOURCE_LOCK "test-project-consumption")
+
+  if (DEFINED ARGS_OUT_TEST_NAME)
+    set(${ARGS_OUT_TEST_NAME} ${test_name} PARENT_SCOPE)
+  endif ()
+
+  if (DEFINED ARGS_DEPENDS)
+    set_tests_properties(${test_name} PROPERTIES DEPENDS "${ARGS_DEPENDS}")
+  endif ()
 endfunction()
 
 
 function(_add_use_project project_name)
+  jcm_parse_arguments(MULTI_VALUE_KEYWORDS "DEPENDS" ARGUMENTS "${ARGN}")
+  set(test_name ${project_name}-add-use)
+
   add_test(
-    NAME ${project_name}-add-use
+    NAME ${test_name}
     COMMAND
     "${CMAKE_CTEST_COMMAND}"
     --verbose
@@ -103,10 +130,23 @@ function(_add_use_project project_name)
     "-D test_name=add-use-${project_name}"
     "-D components=${ARGN}"
     --test-command "${CMAKE_CTEST_COMMAND}")
+
+  set_tests_properties(${test_name} PROPERTIES RESOURCE_LOCK "test-project-consumption")
+
+  if (DEFINED ARGS_DEPENDS)
+    set_tests_properties(${test_name} PROPERTIES DEPENDS "${ARGS_DEPENDS}")
+  endif ()
 endfunction()
 
 function(_file_exists project_name test_suffix file_path)
+  jcm_parse_arguments(MULTI_VALUE_KEYWORDS "DEPENDS" ARGUMENTS "${ARGN}")
+  set(test_name "${project_name}${test_suffix}")
+
   add_test(
-    NAME "${project_name}${test_suffix}"
+    NAME ${test_name}
     COMMAND "${CMAKE_COMMAND}" -E rename "${file_path}" "${file_path}")
+
+  if (DEFINED ARGS_DEPENDS)
+    set_tests_properties(${test_name} PROPERTIES DEPENDS "${ARGS_DEPENDS}")
+  endif ()
 endfunction()
