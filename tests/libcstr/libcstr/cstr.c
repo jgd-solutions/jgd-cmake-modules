f02bb8f2daaa8903b28042990066a65b04dece2c
Justen Di Ruscio <justend29@gmail.com>
2022-11-30 22:18:15 -0400

    ignore MSVC warning to use strcpy_s

--- tests/libcstr/libcstr/cstr.c
+++ tests/libcstr/libcstr/cstr.c
@@ -4,13 +4,26 @@
 
 static const size_t growth_rate = 2;
 
+static void _strcpy(char *restrict dest, const char *restrict src) {
+#ifdef _MSC_VER
+  #pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
+
+  strcpy(dest, src);
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+}
+
 Cstr cstr_create(const char* const init)
 {
   const size_t size = strlen(init);
   const size_t capacity = size * growth_rate + 1;
   char* const data = malloc(capacity);
 
-  strcpy(data, init);
+  _strcpy(data, init);
   data[size] = '\0';
 
   Cstr result = {.data = data, .capacity = capacity, .size = size};
@@ -46,6 +59,6 @@ void cstr_append(Cstr* const str, char c)
 void cstr_copy(Cstr* to, const Cstr* from)
 {
   cstr_reserve(to, from->size);
-  strcpy(to->data, from->data);
+  _strcpy(to->data, from->data);
   to->size = from->size;
 }
