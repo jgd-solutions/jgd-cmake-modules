4cd395f41d461378194624a1e455db28fe99df1d
Justen Di Ruscio <justend29@gmail.com>
2024-03-11 16:23:44 -0300

    attempt to use powershell on windows

--- jgd-cmake-modules/JcmArbitraryScript.cmake
+++ jgd-cmake-modules/JcmArbitraryScript.cmake
@@ -55,6 +55,7 @@ if(CMAKE_SCRIPT_MODE_FILE STREQUAL CMAKE_CURRENT_LIST_FILE)
 
   math(EXPR last_arg_idx "${CMAKE_ARGC} - 1")
   math(EXPR separated_arg_count "${last_arg_idx} - ${separator_arg_idx}")
+  set(code "${CMAKE_ARGV${last_arg_idx}}")
   if(separator_arg_idx STREQUAL last_arg_idx)
     message(FATAL_ERROR "${base_invalid_arg_message}" "No arguments follow the separator.")
   elseif(NOT separated_arg_count STREQUAL "1")
@@ -64,7 +65,7 @@ if(CMAKE_SCRIPT_MODE_FILE STREQUAL CMAKE_CURRENT_LIST_FILE)
 
   # Evaluate code
   # interpret all escape sequences as their control characters
-  string(REPLACE \\n \n code ${CMAKE_ARGV${last_arg_idx}})
+  string(REPLACE \\n \n code "${code}")
   string(REPLACE \\r \r code "${code}")
   string(REPLACE \\t \t code "${code}")
   string(REPLACE \\' ' code "${code}")
@@ -165,6 +166,7 @@ Examples
 #]=======================================================================]
 function(jcm_form_arbitrary_script_command)
   jcm_parse_arguments(
+    OPTIONS "WITHOUT_WINDOWS_POWERSHELL"
     ONE_VALUE_KEYWORDS "OUT_VAR"
     MULTI_VALUE_KEYWORDS "CODE"
     REQUIRES_ALL "OUT_VAR" "CODE"
@@ -183,7 +185,12 @@ function(jcm_form_arbitrary_script_command)
   string(REPLACE " " "\ " code "${code}")
   string(REPLACE \t \\t code "${code}")
 
-  set(${ARGS_OUT_VAR}
-    "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_FUNCTION_LIST_FILE}" -- "'${code}'" PARENT_SCOPE)
+  if(CMAKE_HOST_SYSTEM_NAME MATCHES "^Windows" AND NOT ARGS_WITHOUT_WINDOWS_POWERSHELL)
+    set(${ARGS_OUT_VAR}
+      "powershell" "-Command" "{ Start-Process ${CMAKE_COMMAND} -Wait -ArgumentList \"-P '${CMAKE_CURRENT_FUNCTION_LIST_FILE}' -- '${code}' }" PARENT_SCOPE)
+  else()
+    set(${ARGS_OUT_VAR}
+      "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_FUNCTION_LIST_FILE}" -- "'${code}'" PARENT_SCOPE)
+  endif()
 endfunction()
 
