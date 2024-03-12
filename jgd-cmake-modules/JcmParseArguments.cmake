4621a6a3792eed5bfd47e28e3515c4bca1332d20
Justen Di Ruscio <justend29@gmail.com>
2024-03-11 23:02:00 -0300

    add inclusivity options to jcm_parse_arguments

--- jgd-cmake-modules/JcmParseArguments.cmake
+++ jgd-cmake-modules/JcmParseArguments.cmake
@@ -32,6 +32,8 @@ jcm_parse_arguments
       [REQUIRES_ANY_<n> <keyword>...]
       [MUTUALLY_EXCLUSIVE <keyword>..]
       [MUTUALLY_EXCLUSIVE_<n> <keyword>..]
+      [MUTUALLY_INCLUSIVE <keyword>..]
+      [MUTUALLY_INCLUSIVE_<n> <keyword>..]
       ARGUMENTS <arg>...)
 
 
@@ -87,22 +89,29 @@ Multi Value
 :cmake:variable:`REQUIRES_ANY_<n>`
   Where *n* is an integer in the range [1,3], these three parameters provide the exact same
   functionality as :cmake:variable:`REQUIRES_ANY`, but through separate variables so multiple
-  independent constraints can be provided simultaneously. Each constraints is independently
+  independent constraints can be enforced simultaneously. Each constraints is independently
   verified.
 
 :cmake:variable:`MUTUALLY_EXCLUSIVE`
-  A list of keywords from any of the above keyword lists that must one one keyword present. If
-  :cmake:variable:`ARGUMENTS` includes more than one of the keywords listed here, parsing will emit
-  an error.
+  A list of keywords from any of the above keyword lists that are restricted from being provided
+  simultaneously. If :cmake:variable:`ARGUMENTS` includes more than one of the keywords listed 
+  here, parsing will emit an error.
 
 :cmake:variable:`MUTUALLY_EXCLUSIVE_<n>`
   Where *n* is an integer in the range [1,3], these three parameters provide the exact same
   functionality as :cmake:variable:`MUTUALLY_EXCLUSIVE`, but through separate variables so multiple
-  exclusivity constraints can be provided simultaneously. Each constraint is independently verified.
+  exclusivity constraints can be enforced simultaneously. Each constraint is independently verified.
 
-  A list of keywords from any of the above keyword lists that must one one keyword present. If
-  :cmake:variable:`ARGUMENTS` includes more than one of the keywords listed here, parsing will emit
-  an error.
+:cmake:variable:`MUTUALLY_INCLUSIVE`
+  A list of keywords from any of the above keyword lists that must be provided together; the
+  presence of any keyword in this list triggers the requirement of every keyword in this list.
+  Should :cmake:variable:`ARGUMENTS` include one of the keywords listed here but not all of them, 
+  parsing will emit an error.
+
+:cmake:variable:`MUTUALLY_INCLUSIVE_<n>`
+  Where *n* is an integer in the range [1,3], these three parameters provide the exact same
+  functionality as :cmake:variable:`MUTUALLY_INCLUSIVE`, but through separate variables so multiple
+  inclusivity constraints can be enforced simultaneously. Each constraint is independently verified.
 
 Examples
 ########
@@ -168,7 +177,7 @@ macro(JCM_PARSE_ARGUMENTS)
     # one-value
     "PREFIX"
     # multi-value
-    [[ARGUMENTS;OPTIONS;ONE_VALUE_KEYWORDS;MULTI_VALUE_KEYWORDS;REQUIRES_ALL;REQUIRES_ANY;REQUIRES_ANY_1;REQUIRES_ANY_2;REQUIRES_ANY_3;MUTUALLY_EXCLUSIVE;MUTUALLY_EXCLUSIVE_1;MUTUALLY_EXCLUSIVE_2;MUTUALLY_EXCLUSIVE_3]]
+    [[ARGUMENTS;OPTIONS;ONE_VALUE_KEYWORDS;MULTI_VALUE_KEYWORDS;REQUIRES_ALL;REQUIRES_ANY;REQUIRES_ANY_1;REQUIRES_ANY_2;REQUIRES_ANY_3;MUTUALLY_EXCLUSIVE;MUTUALLY_EXCLUSIVE_1;MUTUALLY_EXCLUSIVE_2;MUTUALLY_EXCLUSIVE_3;MUTUALLY_INCLUSIVE;MUTUALLY_INCLUSIVE_1;MUTUALLY_INCLUSIVE_2;MUTUALLY_INCLUSIVE_3]]
     # function arguments
     "${ARGN}")
 
@@ -241,33 +250,60 @@ macro(JCM_PARSE_ARGUMENTS)
 
 
   # validate keywords that are mutually exclusive
-  unset(first_keyword)
-  unset(second_keyword)
-
-  foreach(keyword IN LISTS
+  foreach(mutex_list IN ITEMS
     INS_MUTUALLY_EXCLUSIVE
     INS_MUTUALLY_EXCLUSIVE_1
     INS_MUTUALLY_EXCLUSIVE_2
     INS_MUTUALLY_EXCLUSIVE_3)
 
+    unset(first_keyword)
+    unset(second_keyword)
+
     # foreach IN LISTS won't execute body if list isn't defined :)
-    if("${keyword}" IN_LIST INS_ARGUMENT)
+    foreach(keyword IN LISTS ${mutex_list})
+      if(NOT "${keyword}" IN_LIST INS_ARGUMENTS)
+        continue()
+      endif()
+
       if(DEFINED first_keyword)
         set(second_keyword ${keyword})
+        set(violated_mutex_list ${mutex_list})
         break()
       else()
         set(first_keyword ${keyword})
       endif()
+    endforeach()
+    
+    if(DEFINED second_keyword)
+      message(FATAL_ERROR
+        "The keywords ${first_keyword} and ${second_keyword} were both defined but are part of the "
+        "mutually exclusive list of function arguments, ${mutex_list}: ${${mutex_list}}")
     endif()
   endforeach()
 
-  if(DEFINED second_keyword)
-    message(FATAL_ERROR "The keywords ${first_keyword} and ${second_keyword} were both defined but are part of the "
-      "mutually exclusive list of function arguments: ${INS_MUTUALLY_EXCLUSIVE}")
-  endif()
 
-  unset(second_keyword)
   unset(first_keyword)
+  unset(second_keyword)
+
+  # validate keywords that are mutually inclusive
+  foreach(inclusive_list IN ITEMS 
+    INS_MUTUALLY_INCLUSIVE
+    INS_MUTUALLY_INCLUSIVE_1
+    INS_MUTUALLY_INCLUSIVE_2
+    INS_MUTUALLY_INCLUSIVE_3)
+
+    if(NOT DEFINED ${inclusive_list})
+      continue()
+    endif()
+
+    set(missing_keywords "${${inclusive_list}}")
+    list(REMOVE_ITEM missing_keywords "${INS_ARGUMENTS}") 
+    if(missing_keywords)
+      message(FATAL_ERROR
+        "The keywords were not present '${missing_keywords}' but are specified as part of the "
+        "mutually inclusive list of arguments ${inclusive_list}: ${${inclusive_list}}")
+    endif()
+  endforeach()
 
   # validate caller's argument format
   if(NOT INS_WITHOUT_MISSING_VALUES_CHECK AND ${INS_PREFIX}_KEYWORDS_MISSING_VALUES)
