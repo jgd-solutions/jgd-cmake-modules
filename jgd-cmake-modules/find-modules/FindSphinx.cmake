9a05e9dc6ae7996907401458a8c3f25e33b789bd
Justen Di Ruscio <justend29@gmail.com>
2026-01-18 10:16:42 -0400

    update FindSphinx w/ block(), fixed docs, standardargs

--- jgd-cmake-modules/find-modules/FindSphinx.cmake
+++ jgd-cmake-modules/find-modules/FindSphinx.cmake
@@ -15,7 +15,9 @@ Cache Variables
 ~~~~~~~~~~~~~~~~
 
 :cmake:variable:`Sphinx_EXECUTABLE`
-  Path to found sphinx build executable
+  Absolute path to the found sphinx-build executable, used to minimize repeated searches with
+  repeated find_package(Sphinx) calls. This value can be queried from the target's
+  *IMPORTED_LOCATION* property.
 
 Result Variables
 ~~~~~~~~~~~~~~~~
@@ -48,73 +50,71 @@ Examples
 
   find_package(Sphinx REQUIRED)
 
+  jcm_create_sphinx_targets(CONFIGURE_CONF_PY)
+
 #]=======================================================================]
 
 include(FindPackageHandleStandardArgs)
 
+block(SCOPE_FOR VARIABLES PROPAGATE Sphinx_FOUND Sphinx_VERSION Sphinx_VERSION_MAJOR Sphinx_VERSION_MINOR Sphinx_VERSION_PATCH)
+
 # use python interpreter path as a base of search
 find_package(Python COMPONENTS Interpreter)
 if(Python_Interpreter_FOUND)
-  cmake_path(GET Python_EXECUTABLE PARENT_PATH _Python_interp_dir)
-  set(_Sphinx_bin_hint "${_Python_interp_dir}")
-  set(_Sphinx_scripts_hint "${_Python_interp_dir}/Scripts")
-  unset(_Python_interp_dir)
+  cmake_path(GET Python_EXECUTABLE PARENT_PATH python_interp_dir)
+  set(bin_hint "${python_interp_dir}")
+  set(scripts_hint "${python_interp_dir}/Scripts")
 else()
-  unset(_Sphinx_bin_hint)
-  unset(_Sphinx_scripts_hint)
+  unset(bin_hint)
+  unset(scripts_hint)
 endif()
 
 find_program(
   Sphinx_EXECUTABLE
   NAMES sphinx-build sphinx-build2 sphinx-build3
-  HINTS "${_Sphinx_bin_hint}" "${_Sphinx_scripts_hint}"
+  HINTS "${bin_hint}" "${scripts_hint}"
   DOC "Path to Sphinx documentation builder executable")
-unset(_Sphinx_bin_hint)
-unset(_Sphinx_scripts_hint)
 mark_as_advanced(Sphinx_EXECUTABLE)
 
 # executable version
 if(Sphinx_EXECUTABLE)
   execute_process(
     COMMAND "${Sphinx_EXECUTABLE}" --version
-    OUTPUT_VARIABLE _Sphinx_version_stdout
-    ERROR_VARIABLE _Sphinx_version_stderr
+    OUTPUT_VARIABLE version_stdout
+    ERROR_VARIABLE version_stderr
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  if(_Sphinx_version_stderr)
+  if(version_stderr)
     message(WARNING
       "Failed to determine version of sphinx build executable (${Sphinx_EXECUTABLE})! Ensure any "
-      "pertinent Python virtual environment is activated. Error: ${_Sphinx_version_stderr}")
-  elseif(NOT _Sphinx_version_stdout MATCHES "sphinx-build[23]? [0-9]+\\.[0-9]+\\.[0-9]+")
+      "pertinent Python virtual environment is activated. Error: ${version_stderr}")
+  elseif(NOT version_stdout MATCHES "sphinx-build[23]? [0-9]+\\.[0-9]+\\.[0-9]+")
     message(WARNING
-      "Sphinx's version output is not recognized by this find module (${_Sphinx_version_stdout})!)")
+      "Sphinx's version output is not recognized by this find module (${version_stdout})!)")
   else()
     # extract version from stdout
-    string(REGEX REPLACE ".*sphinx-build[23]? " "" Sphinx_VERSION "${_Sphinx_version_stdout}")
-    string(REPLACE "." ";" _Sphinx_version_components "${Sphinx_VERSION}")
-    list(GET _Sphinx_version_components 0 Sphinx_VERSION_MAJOR)
-    list(GET _Sphinx_version_components 1 Sphinx_VERSION_MINOR)
-    list(GET _Sphinx_version_components 2 Sphinx_VERSION_PATCH)
+    string(REGEX REPLACE ".*sphinx-build[23]? " "" Sphinx_VERSION "${version_stdout}")
+    string(REPLACE "." ";" version_components "${Sphinx_VERSION}")
+    list(GET version_components 0 Sphinx_VERSION_MAJOR)
+    list(GET version_components 1 Sphinx_VERSION_MINOR)
+    list(GET version_components 2 Sphinx_VERSION_PATCH)
   endif()
-
-  unset(_Sphinx_version_components)
-  unset(_Sphinx_version_stderr)
-  unset(_Sphinx_version_stdout)
 endif()
 
-string(CONCAT _Sphinx_failure_message
+string(CONCAT failure_message
   "Sphinx is not installed or a Python virtual environment may not have been activated.\n"
   "'Sphinx_ROOT' can be set to refer to a Sphinx installation root, even within a virtual env. "
   "Ex. `cmake -B build -D Sphinx_ROOT=/path/to/.venv/bin`")
 
+# Sphinx_FOUND and SPHINX_FOUND automatically set
 find_package_handle_standard_args(Sphinx
-  FOUND_VAR Sphinx_FOUND
   REQUIRED_VARS Sphinx_EXECUTABLE
   VERSION_VAR Sphinx_VERSION
-  REASON_FAILURE_MESSAGE ${_Sphinx_failure_message})
-unset(_Sphinx_failure_message)
+  REASON_FAILURE_MESSAGE ${failure_message})
 
 if(Sphinx_FOUND AND NOT TARGET Sphinx::build)
   add_executable(Sphinx::build IMPORTED)
   set_target_properties(Sphinx::build PROPERTIES IMPORTED_LOCATION "${Sphinx_EXECUTABLE}")
 endif()
+
+endblock()
