a20798678b00774878e69cbb12d5213d431ab44c
Justen Di Ruscio <justend29@gmail.com>
2026-01-18 10:27:05 -0400

    update Findliburing w/ fixed docs, cache vars, simplified

--- jgd-cmake-modules/find-modules/Findliburing.cmake
+++ jgd-cmake-modules/find-modules/Findliburing.cmake
@@ -17,10 +17,21 @@ searching for liburing on non-Linux machines, explicitly wrap the
 Cache Variables
 ~~~~~~~~~~~~~~~~
 
-None
+:cmake:variable:`liburing_INCLUDE_DIR`
+  Absolute path to the found liburing include directory, used to minimize repeated searches
+  with repeated find_package(liburing) calls. This value can be queried from the target's
+  *INTERFACE_INCLUDE_DIRECTORIES* property.
+
+:cmake:variable:`liburing_LIBRARY`
+  Absolute path to the found liburing shared/static library, used to minimize repeated searches with
+  repeated find_package(liburing) calls. This value can be queried from the target's
+  *IMPORTED_LOCATION* property.
 
 Result Variables
 ~~~~~~~~~~~~~~~~
+
+:cmake:variable:`liburing_FOUND`
+  Boolean indicating whether the requested version of liburing was found.
 
 :cmake:variable:`liburing_VERSION`
   The found liburing library version in the form *<major>.<minor>*
@@ -55,55 +66,56 @@ Examples
 
 #]=======================================================================]
 
-block(PROPAGATE liburing_VERSION liburing_VERSION_MAJOR liburing_VERSION_MINOR)
-  find_path(liburing_INCLUDE_DIR NAMES liburing.h NO_CACHE)
-  if(NOT liburing_INCLUDE_DIR)
-    return()
-  endif()
+include(FindPackageHandleStandardArgs)
 
-  find_library(liburing_LIBRARY NAMES uring NO_CACHE)
-  if(NOT liburing_LIBRARY)
-    return()
-  endif()
+block(PROPAGATE liburing_FOUND liburing_VERSION liburing_VERSION_MAJOR liburing_VERSION_MINOR)
 
-  set(liburing_version_file "${liburing_INCLUDE_DIR}/liburing/io_uring_version.h")
-  if(EXISTS "${liburing_version_file}") 
-    # version components defined in the form:
-    #define IO_URING_VERSION_MAJOR 2
-    #define IO_URING_VERSION_MINOR 11
+find_path(liburing_INCLUDE_DIR NAMES liburing.h)
+if(NOT liburing_INCLUDE_DIR)
+  return()
+endif()
 
-    file(STRINGS "${liburing_version_file}" versions REGEX "IO_URING_VERSION_")
-    list(LENGTH versions num_version_components)
-    if(num_version_components EQUAL 2)
-      list(TRANSFORM versions REPLACE "[^0-9]" "")
-      list(GET versions 0 liburing_VERSION_MAJOR)
-      list(GET versions 1 liburing_VERSION_MINOR)
-      string(JOIN "." liburing_VERSION ${versions})
-      set(liburing_find_version_args VERSION_VAR;liburing_VERSION;HANDLE_VERSION_RANGE)
-    else()
-      message(WARNING
-        "Found liburing version file, but it contains unrecognized version format "
-        "'${liburing_version_file}': ${versions}")
-    endif()
+find_library(liburing_LIBRARY NAMES uring)
+if(NOT liburing_LIBRARY)
+  return()
+endif()
 
+set(liburing_version_file "${liburing_INCLUDE_DIR}/liburing/io_uring_version.h")
+if(EXISTS "${liburing_version_file}") 
+  # version components defined in the form:
+  #define IO_URING_VERSION_MAJOR 2
+  #define IO_URING_VERSION_MINOR 11
+
+  file(STRINGS "${liburing_version_file}" versions REGEX "IO_URING_VERSION_")
+  list(LENGTH versions num_version_components)
+  if(num_version_components EQUAL 2)
+    list(TRANSFORM versions REPLACE "[^0-9]" "")
+    list(GET versions 0 liburing_VERSION_MAJOR)
+    list(GET versions 1 liburing_VERSION_MINOR)
+    string(JOIN "." liburing_VERSION ${versions})
   else()
-    unset(liburing_VERSION)
-    unset(liburing_find_version_args)
+    message(WARNING
+      "Found liburing version file, but it contains unrecognized version format "
+      "'${liburing_version_file}': ${versions}")
   endif()
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(
-    liburing 
-    ${liburing_find_version_args}
-    REQUIRED_VARS liburing_LIBRARY liburing_INCLUDE_DIR)
-  if(NOT LIBURING_FOUND)
-    return()
-  endif()
+else()
+  unset(liburing_VERSION)
+endif()
 
-  add_library(liburing_liburing IMPORTED UNKNOWN)
-  add_library(liburing::liburing ALIAS liburing_liburing)
-  set_target_properties(liburing_liburing 
+# liburing_FOUND and LIBURING_FOUND automatically set
+find_package_handle_standard_args(
+  liburing 
+  VERSION_VAR liburing_VERSION
+  HANDLE_VERSION_RANGE
+  REQUIRED_VARS liburing_LIBRARY liburing_INCLUDE_DIR)
+
+if(liburing_FOUND AND NOT TARGET liburing::liburing)
+  add_library(liburing::liburing IMPORTED UNKNOWN)
+  set_target_properties(liburing::liburing 
     PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES "${liburing_INCLUDE_DIR}"
       IMPORTED_LOCATION "${liburing_LIBRARY}")
+endif()
+
 endblock()
